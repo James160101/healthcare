d2d43d9faa26c2adcc5734500bfc0476013ebe17
James <josemswernerlodwige@gmail.com>
2025-11-14 09:53:58 +0300

    version 6

--- healthcare/lib/screens/auth/auth_wrapper.dart
+++ healthcare/lib/screens/auth/auth_wrapper.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:provider/provider.dart'; // Ajout de l'import Provider
+import 'package:provider/provider.dart';
 import '../../services/firebase_service.dart';
 import 'login_screen.dart';
 import '../main_screen.dart';
@@ -10,7 +10,6 @@ class AuthWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Correction: Utilisation de l'instance de FirebaseService fournie par Provider
     final firebaseService = Provider.of<FirebaseService>(context, listen: false);
 
     return StreamBuilder<User?>(
@@ -25,10 +24,10 @@ class AuthWrapper extends StatelessWidget {
         }
 
         if (snapshot.hasData && snapshot.data != null) {
-          return const MainScreen(); // Redirige vers MainScreen
+          return const MainScreen(); // Si l'utilisateur est connecté, aller à l'écran principal
         }
 
-        return const LoginScreen();
+        return const LoginScreen(); // Sinon, afficher l'écran de connexion
       },
     );
   }
